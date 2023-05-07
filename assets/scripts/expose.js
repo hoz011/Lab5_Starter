54849c76639ab1503693bd7a02e873236e60bd81
hoz011 <114896112+hollyzhu573@users.noreply.github.com>
2023-05-06 18:24:12 -0700

    Set images

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -16,6 +16,10 @@ console.log(main_img.src);
 horn_select.addEventListener("change", (event) => {
   if (event.target.value == 'party-horn') {
     main_img.src = "assets/images/party-horn.svg";
+  } else if (event.target.value == 'air-horn') {
+    main_img.src = "assets/images/air-horn.svg";
+  } else if (event.target.value == 'car-horn') {
+    main_img.src = "assets/images/car-horn.svg";
   }
 });
 
