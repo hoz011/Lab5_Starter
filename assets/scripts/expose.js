8e58c78503a6351a7ddd533fd5264db4400ed6e7
hoz011 <114896112+hollyzhu573@users.noreply.github.com>
2023-05-06 17:36:28 -0700

    Horn image on change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,5 +4,15 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const horn_select = document.getElementById("horn-select");
+  var main_img = document.querySelector("img");
 }
+
+horn_select.addEventListener("change", (event) => {
+  if (EventTarget.value == 'party-horn') {
+    main_img.src = "assets/images/party-horn.svg";
+  }
+});
+
+
 
