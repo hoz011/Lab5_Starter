f24d9eac1629d8c666f1450507cf37e795933614
hoz011 <114896112+hollyzhu573@users.noreply.github.com>
2023-05-07 17:44:39 -0700

    Set audio files

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -10,18 +10,24 @@ function init() {
 
 const horn_select = document.getElementById("horn-select");
 var main_img = document.querySelector("img");
+var main_audio = document.querySelector("audio");
 console.log(horn_select);
 console.log(main_img.src);
 
 horn_select.addEventListener("change", (event) => {
   if (event.target.value == 'party-horn') {
     main_img.src = "assets/images/party-horn.svg";
+    main_audio.src = "assets/audio/party-horn.mp3";
   } else if (event.target.value == 'air-horn') {
     main_img.src = "assets/images/air-horn.svg";
+    main_audio.src = "assets/audio/air-horn.mp3";
   } else if (event.target.value == 'car-horn') {
     main_img.src = "assets/images/car-horn.svg";
+    main_audio.src = "assets/audio/car-horn.mp3";
   }
 });
+
+
 
 
 
